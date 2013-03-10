fce8e38e15ad76c1adf1629eabf04910310cf30d
mogproject <mogproj@gmail.com>
2013-03-11 02:45:03 +0900

    Refactored.

--- src/ScalaConfJP2013Challenge.scala
+++ src/ScalaConfJP2013Challenge.scala
@@ -1,44 +1,24 @@
-import annotation.tailrec
-
 object ScalaConfJP2013Challenge {
   def quiz1(intersections: Array[String]): Int = {
-    val initUsed =intersections.map(_.toArray.map(_.toString == "1"))
-    val n = initUsed.size
-    val m = initUsed(0).size
+    val initState = intersections.map(_.map(_ == '0').toArray)
+    val n = initState.size
+    val m = initState(0).size
 
-    // TODO: Refactor to tail recursive.
-    def f(i: Int, j: Int, used: Array[Array[Boolean]], route: List[(Int, Int)]): Int = (i, j) match {
-      case _ if i < 0 || j < 0 || i >= n || j >= m => 0
-      case _ if used(i)(j) => 0
-      case _ if i == n - 1 && j == m - 1 => {
-//        println((i, j) :: route)  // Print all routes.
-        1
-      }
-      case _ =>
-        List((0, 1), (1, 0), (0, -1), (-1, 0)).map{
-          d =>
-            used(i)(j) = true
-            val ret = f(i + d._1, j + d._2, used, (i, j) :: route)
-            used(i)(j) = false
-            ret
-        }.sum
+    def f(x: Int, y: Int, state: Array[Array[Boolean]]): Int = {
+      if (x < 0 || x >= n || y < 0 || y >= m || ! state(x)(y)) return 0
+      if (x == n - 1 && y == m - 1) return 1
+      val nextState = for (i <- state) yield {for (j <- i) yield j}
+      nextState(x)(y) = false
+      List((0, 1), (1, 0), (0, -1), (-1, 0)).map {
+        d => f(x + d._1, y + d._2, nextState)
+      }.sum
     }
-    f(0, 0, initUsed, Nil)
+    f(0, 0, initState)
   }
 
   def quiz2(chestnuts: Array[String]): Int = {
     val a = chestnuts.map(_.toArray.map(_.toString.toInt))
-
-    // TODO: Refactor to tail recursive.
-    def f(i: Int, j: Int): Int = {
-      val x = (i, j) match {
-        case (0, 0) => 0
-        case (_, 0) => f(i - 1, 0)
-        case (0, _) => f(0, j - 1)
-        case _ => List(f(i - 1, j), f(i, j - 1)).max
-      }
-      x + a(i)(j)
-    }
+    def f(i: Int, j: Int): Int = if (i >= 0 && j >= 0) math.max(f(i - 1, j), f(i, j - 1)) + a(i)(j) else 0
     f(a.size - 1, a(0).size - 1)
   }
 
@@ -48,31 +28,27 @@ object ScalaConfJP2013Challenge {
       val stations = edges.map(_._1).toSet ++ edges.map(_._2).toSet
       val INF = 9999
 
+      // Initialize.
       val m: collection.mutable.Map[(String, String), Int] = collection.mutable.Map()
       val route: collection.mutable.Map[(String, String), List[String] ] = collection.mutable.Map()
-      for (i <- stations)
-        for (j <- stations) {
-          m.put((i, j), if (i == j) 0 else INF)
-          route.put((i, j), Nil)
-        }
-
-      for (i <- edges) {
-        m.update((i._1, i._2), i._3)
+      for (i <- stations; j <- stations) {
+        m.put((i, j), if (i == j) 0 else INF)
+        route.put((i, j), Nil)
       }
 
-      // Create graph.
-      for (k <- stations)
-        for (i <- stations)
-          for (j <- stations) {
-            val x: Int = m.getOrElse((i, j), 0)
-            val y = m.getOrElse((i, k), 0) + m.getOrElse((k, j), 0)
-            if (x > y) {
-              m.update((i, j), y)
-              route.update((i, j),
-                route.getOrElse(
-                  (i, k), Nil) ::: List(k) ::: route.getOrElse((k, j), Nil))
-            }
-          }
+      for (i <- edges) m.update((i._1, i._2), i._3)
+
+      // Use Warshall-Floyd Algorithm.
+      for (k <- stations; i <- stations; j <- stations) {
+        val x: Int = m.getOrElse((i, j), 0)
+        val y = m.getOrElse((i, k), 0) + m.getOrElse((k, j), 0)
+        if (x > y) {
+          m.update((i, j), y)
+          route.update((i, j),
+            route.getOrElse(
+              (i, k), Nil) ::: List(k) ::: route.getOrElse((k, j), Nil))
+        }
+      }
 
       val src = "たかし家"
       val dst = "市場前"
@@ -83,9 +59,7 @@ object ScalaConfJP2013Challenge {
 
     val shortest = f(rails.map(i => (i._1, i._2, i._3)))
     val cheapest = f(rails.map(i => (i._1, i._2, i._4)))
-
-    "%s: %d km\n%s: %d yen".format(
-      shortest._2, shortest._1, cheapest._2, cheapest._1)
+    "%s: %d km\n%s: %d yen".format(shortest._2, shortest._1, cheapest._2, cheapest._1)
   }
 
   def main(args: Array[String]) {
@@ -98,7 +72,7 @@ object ScalaConfJP2013Challenge {
       "01000"
     )
     println(quiz1(arg1))
-    assert(quiz1(arg1) == 80)
+    assume(quiz1(arg1) == 80)
 
     // Question 2.
     val arg2 = Array(
@@ -112,7 +86,7 @@ object ScalaConfJP2013Challenge {
       "97739214"
     )
     println(quiz2(arg2))
-    assert(quiz2(arg2) == 86)
+    assume(quiz2(arg2) == 86)
 
     // Question 3.
     val arg3 = Array(
@@ -127,7 +101,8 @@ object ScalaConfJP2013Challenge {
       ("小森", "市場前", 5, 140)
     )
     println(quiz3(arg3))
-    assert(quiz3(arg3) == "たかし家 -> 御湯ノ水 -> 夏葉原 -> 小森 -> 市場前: 40 km\n" +
+    assume(quiz3(arg3) ==
+      "たかし家 -> 御湯ノ水 -> 夏葉原 -> 小森 -> 市場前: 40 km\n" +
       "たかし家 -> 御湯ノ水 -> 夏葉原 -> 小森 -> 市場前: 590 yen")
   }
 }
